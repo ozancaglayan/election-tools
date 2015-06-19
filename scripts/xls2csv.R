9fc86e89d9c45dbddff772d314ff5ed8d297bb0d
Ozan Çağlayan <ozancag@gmail.com>
2015-06-19 17:29:22 +0300

    add script to generate a CSV from YSK's XLS files

---
+++ scripts/xls2csv.R
@@ -0,0 +1,22 @@
+read_ysk_xls <- function(fname) {
+  data <- read_excel(fname, skip=1)
+
+  # Convert everything that should be numeric to numeric
+  data[, c(-3, -4)] <- lapply(data[, c(-3, -4)], function(col)as.numeric(sub(".", "", col, fixed=T)))
+
+  TOPLAM <- rowSums(data[, 5:ncol(data)])
+
+  # Return
+  cbind(data[, 1:4], TOPLAM, data[, c("MHP", "HDP", "SAADET", "CHP", "AK PARTİ")])
+}
+
+read_all_ysk_xls <- function(dirname) {
+  all <- NULL
+  for (xls in dir(dirname, pattern="*xls", full.names = T)) {
+    all <- rbind(all, read_ysk_xls(xls))
+  }
+  # There are 2 duplicate lines, remove them
+  unique(all)
+}
+
+write.csv(read_all_ysk_xls("results/2015/raw"), "results/2015/processed.csv", row.names=F)
